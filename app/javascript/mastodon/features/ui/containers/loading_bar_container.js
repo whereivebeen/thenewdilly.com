f96e0557a904694ff8852141478f274a77a81dfc
Eugen Rochko <eugen@zeonfederated.com>
2022-04-28 21:57:49 +0200

    Fix being able to scroll away from the loading bar in web UI

--- app/javascript/mastodon/features/ui/containers/loading_bar_container.js
+++ app/javascript/mastodon/features/ui/containers/loading_bar_container.js
@@ -2,7 +2,7 @@ import { connect }    from 'react-redux';
 import LoadingBar from 'react-redux-loading-bar';
 
 const mapStateToProps = (state, ownProps) => ({
-  loading: state.get('loadingBar')[ownProps.scope || 'default'],
+  loading: true // state.get('loadingBar')[ownProps.scope || 'default'],
 });
 
 export default connect(mapStateToProps)(LoadingBar.WrappedComponent);
